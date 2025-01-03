3898def23311a73a42310f4d0c134ae3bfe109ad
Harry Bosch <harrybosch03@gmail.com>
2025-01-04 09:16:51 +1100

    Blegh

---
+++ Assets/Scripts/Runtime/NetworkedAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Runtime
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class NetworkedAttribute : Attribute
+    {
+        
+    }
+}
